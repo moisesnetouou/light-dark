a50089bc259ea9641b0bc89f150a0ccd800a95f4
Moisés Neto <moisesnetored@gmail.com>
2021-09-08 12:00:32 -0400

    style: Card criado.

--- src/Card/index.js
+++ src/Card/index.js
@@ -1,7 +1,33 @@
+import { Button } from "@chakra-ui/button";
+import { Flex, Heading, Text } from "@chakra-ui/layout";
+
 export function Card(){
   return(
-    <div>
-      <p>Teste</p>
-    </div>
+    <Flex 
+      w="100%" 
+      h="100vh"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Flex
+      bg="white"
+      w="35rem"
+      h="20rem"
+      flexDir="column"
+      justifyContent="flex-start"
+      p="10"
+      borderRadius="10"
+    >
+      <Heading marginBottom="6">Lorem Ipsum</Heading>
+
+      <Text marginBottom="6">Lorem, ipsum dolor sit amet consectetur adipisicing.
+        Isquamr rtempore molestias  totam incidunt.
+      </Text>
+
+      <Button bg="#28B2FF" w="20rem" marginTop="10">
+        Trocar Tema
+      </Button>
+    </Flex>
+    </Flex>
   );
 }
